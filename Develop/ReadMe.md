e215ed318c4c4ce55dc0ea2fd9b0be4b5c4b3eed
Amalia Hay <amaliawhay@gmail.com>
2020-07-08 18:54:05 -0700

    write questions to readMe file

---
+++ Develop/ReadMe.md
@@ -0,0 +1,20 @@
+
+        
+  ## Title
+  * test
+  ## Description
+  * test
+  ## Table of Contents
+  * test
+  ## Installation
+  * test
+  ## Usage
+  * 
+  ## License
+  * test
+  ## Contributing
+  * test
+  ## Tests
+  * test
+  ## Questions
+  * 
--- Develop/index.js
+++ Develop/index.js
@@ -1,62 +1,93 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
 // array of questions for user
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is your project called?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Enter a description of your project.",
-  },
-  { type: "input" },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project called?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description of your project.",
+    },
+    {
+      type: "input",
+      name: "tableOfContents",
+      message:
+        "What would you like in your table of contents?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How is your project installed?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Describe the ways to use your project",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Enter your license information here",
+    },
+    {
+      type: "input",
+      name: "contributions",
+      message: "How can others contribute to your project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Describe how to test this project",
+    },
+  ])
+  .then(function (res) {
+    console.log(res);
+    console.log(res.title);
+    console.log(res.description);
+    console.log(res.tableOfContents);
+    console.log(res.installation);
 
-  {
-    type: "checkbox",
-    name: "table of contents",
-    message: "Select all that apply: ",
-    choices: [
-      "Installation",
-      "Usage",
-      "License",
-      "Contributing",
-      "Tests",
-      "Questions",
-    ],
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "How is your project installed?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Describe the ways to use your project",
-  },
-  {
-    type: "input",
-    name: "license",
-    message: "Enter your license information here",
-  },
-  {
-    type: "input",
-    name: "contributions",
-    message: "How can others contribute to your project?",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "Describe how to test this project",
-  },
-];
+    const md = `
+        
+  ## Title
+  * ${res.title}
+  ## Description
+  * ${res.description}
+  ## Table of Contents
+  * ${res.tableOfContents}
+  ## Installation
+  * ${res.installation}
+  ## Usage
+  * 
+  ## License
+  * ${res.license}
+  ## Contributing
+  * ${res.contributions}
+  ## Tests
+  * ${res.test}
+  ## Questions
+  * `;
 
-// function to write README file
-function writeToFile(fileName, data) {}
+    fs.writeFile("ReadMe.md", md, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("ReadMe.md created!");
+      }
+    });
+  });
 
-// function to initialize program
-function init() {}
+// // function to write README file
+// function writeToFile(fileName, data) {}
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {}
+
+// // function call to initialize program
+// init();
