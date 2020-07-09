717b6b3d186797e94388101041d5b2c0bc939773
Amalia Hay <amaliawhay@gmail.com>
2020-07-08 17:54:03 -0700

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,62 @@
 // array of questions for user
 const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project called?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project.",
+  },
+  { type: "input" },
 
+  {
+    type: "checkbox",
+    name: "table of contents",
+    message: "Select all that apply: ",
+    choices: [
+      "Installation",
+      "Usage",
+      "License",
+      "Contributing",
+      "Tests",
+      "Questions",
+    ],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How is your project installed?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Describe the ways to use your project",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Enter your license information here",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "How can others contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Describe how to test this project",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
